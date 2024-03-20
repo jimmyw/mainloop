6d1a24e4b9eb6d53eeb0f0c17cbd0e2d0be28ed9
Jimmy Wennlund <jimmy@wennlund.nu>
2024-03-20 12:47:31 +0100

    Cleanup

--- mainloop.h
+++ mainloop.h
@@ -12,37 +12,9 @@
 //                              Macros and Typedefs
 // -----------------------------------------------------------------------------
 
-#define MAINLOOP_MAX_TASKS 32
-
-enum mainloop_task_type_e {
-    MAINLOOP_PERIODIC_TASKS,
-    MAINLOOP_ONE_SHOT_TASKS,
-    MAINLOOP_IDLE_TASKS,
-    MAINLOOP_INSTANC_TASK,
-    MAINLOOP_ESP_EVENT,
-};
-
-typedef enum mainloop_task_type_e mainloop_task_type_t;
 typedef void (*mainloop_task_cb_t)(void *ctx);
 typedef uint8_t priority_t;
 
-struct task_s {
-    mainloop_task_type_t task_type;
-    const char *name;
-    mainloop_task_cb_t task;
-    void *task_ctx;
-    priority_t priority;
-    union {
-        uint32_t period_ms;
-        uint32_t delay_ms;
-    } task_data;
-};
-
-struct mainloop_s {
-    const char *name;
-    // Tasks in this array, will always be sorted and executed in priority order
-    struct task_s *tasks;
-};
 
 typedef struct mainloop_s mainloop_t;
 
